1724929775ef2928de80fd1c980b8d328cd43d98
yeongbba <yeongmolee2@gmail.com>
2020-11-06 16:46:15 +0900

    make landingpage

--- server/routes/video.js
+++ server/routes/video.js
@@ -86,4 +86,13 @@ router.post("/thumbnail", (req, res) => {
     });
 });
 
+router.get("/getVideos", (req, res) => {
+  Video.find()
+    .populate("writer")
+    .exec((err, videos) => {
+      if (err) return res.status(400).send(err);
+      res.status(200).json({ success: true, videos });
+    });
+});
+
 module.exports = router;
